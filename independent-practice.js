c1331de5051315766a583e6d2d43554f33b64baa
sam <Tan-Sam@users.noreply.github.com>
2017-05-23 14:16:24 +0800

    filled in question 1-4 + 2.

--- independent-practice.js
+++ independent-practice.js
@@ -3,6 +3,16 @@
 // number is the length of the corresponding string.
 
 // your code here
+var lengths = function(arg) {
+  var resultNumArr = [];
+  arg.forEach(function(someStr){
+    resultNumArr.push(someStr.length);
+  });
+  return resultNumArr;
+}
+console.log(
+  lengths(['a','aa','aaa','aaaa'])
+);
 
 // 2. Write a Javascript function called `transmogrifier`
 // This function should accept three arguments, which you can assume will be numbers.
@@ -12,9 +22,25 @@
 // raised to the power of the third number.
 
 // your code here
+var transmogrifier = function(arg1, arg2, arg3){
+  var transmogrified = Math.pow((arg1* arg2), arg3);
+  return transmogrified;
+}
+
+console.log(
+  transmogrifier(2,2,2)
+);
 
 // 3. Write a function `wordReverse` that accepts a single argument, a string. The
 // method should return a string with the order of the words reversed. Don't worry
 // about punctuation.
 
 // your code here
+var wordReverse = function() {
+
+  return this.split('').reverse().toString().replace(/,/g,'');
+}
+
+String.prototype.wordReverse = wordReverse;
+console.log(
+"Wolo holo".wordReverse());
